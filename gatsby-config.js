1934df77a0ab8601ea03e43572a6a39210d68653
Malachi Ivey <zarekbusiness@gmail.com>
2020-09-27 08:40:58 -0400

    Added contentful query

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,12 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
   ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout.js'
 
 const Blog = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
+        edges {
+          node {
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
+          }
+        }
+      }
+    }
+  `)
     return (
         <Layout>
             <h1>This is the blog page.</h1>
+            <ol>
+                {data.allContentfulBlogPost.edges.map(edge => {
+                return (
+                <li>
+                    <Link to={`/blog/${edge.node.slug}`}>
+                        <h2>{edge.node.title}</h2>
+                        <p>Published {edge.node.publishedDate}</p>
+                    </Link>
+                </li>
+                    )
+                })}
+            </ol>
         </Layout>
     )
 }
